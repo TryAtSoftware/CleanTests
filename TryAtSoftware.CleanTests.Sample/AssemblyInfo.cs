54e8674944c0c135b65649196bfcdc4689cdcceb
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-30 15:34:37 +0200

    Extended the sample with an example of missing assembly with shared utilities

--- TryAtSoftware.CleanTests.Sample/AssemblyInfo.cs
+++ TryAtSoftware.CleanTests.Sample/AssemblyInfo.cs
@@ -2,3 +2,4 @@
 
 [assembly: TestFramework("TryAtSoftware.CleanTests.Core.XUnit.CleanTestFramework", "TryAtSoftware.CleanTests.Core")]
 [assembly: SharesUtilitiesWith("TryAtSoftware.CleanTests.Sample.Mathematics")]
+[assembly: SharesUtilitiesWith("SomeMissingAssembly")]
