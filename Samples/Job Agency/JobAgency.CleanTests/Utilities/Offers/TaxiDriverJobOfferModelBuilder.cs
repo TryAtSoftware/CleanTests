1ed534b319d8bb96bd4d74d30d385484410a1795
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-28 11:19:41 +0200

    Added a new job offer model builder `Taxi driver`.

---
+++ Samples/Job Agency/JobAgency.CleanTests/Utilities/Offers/TaxiDriverJobOfferModelBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace JobAgency.CleanTests.Utilities.Offers;
+
+using JobAgency.CleanTests.Utilities.Constants;
+using TryAtSoftware.CleanTests.Core.Attributes;
+
+[CleanUtility(CleanUtilitiesCategories.JobOffer, "taxi_driver")]
+[ExternalDemands(CleanUtilitiesCategories.JobAgency, JobAgencyCharacteristics.TransportIndustry)]
+public class TaxiDriverJobOfferModelBuilder : BaseJobOfferModelBuilder
+{
+    protected override string GetTitle() => "Taxi driver";
+}
