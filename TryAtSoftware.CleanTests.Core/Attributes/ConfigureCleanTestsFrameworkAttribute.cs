417a637c7c0f1eba8d7abd8b5f35f9fd5309152e
Tony Troeff <tonyslav_troeff@outlook.com>
2023-03-06 22:14:30 +0200

    Added a new configuration parameter that should allow control over the maximum degree of parallelism when executing clean tests

--- TryAtSoftware.CleanTests.Core/Attributes/ConfigureCleanTestsFrameworkAttribute.cs
+++ TryAtSoftware.CleanTests.Core/Attributes/ConfigureCleanTestsFrameworkAttribute.cs
@@ -1,10 +1,22 @@
 ﻿namespace TryAtSoftware.CleanTests.Core.Attributes;
 
 using System;
-using Xunit.Sdk;
+using TryAtSoftware.CleanTests.Core.Internal;
 
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
 public class ConfigureCleanTestsFrameworkAttribute : Attribute
 {
-    public bool UseTraits { get; set; }
+    private int _maxDegreeOfParallelism = CleanTestConstants.MaxDegreeOfParallelism;
+
+    public bool UseTraits { get; set; } = CleanTestConstants.UseTraits;
+
+    public int MaxDegreeOfParallelism
+    {
+        get => this._maxDegreeOfParallelism;
+        set
+        {
+            if (value <= 0) throw new ArgumentException("The value max degree of parallelism should be always positive.");
+            this._maxDegreeOfParallelism = value;
+        }
+    }
 }
