b8d3b5c088b86f788d58f9c1ccc1632a49088d7c
Tony Troeff <tonyslav_troeff@outlook.com>
2023-03-28 10:26:48 +0300

    Added a new component `Combinatorial machine setup` that should be used to represent some known scenarious or cases we want to test/benchmark

---
+++ Tests/TryAtSoftware.CleanTests.UnitTests/Parametrization/CombinatorialMachineSetup.cs
@@ -0,0 +1,101 @@
+﻿namespace TryAtSoftware.CleanTests.UnitTests.Parametrization;
+
+using TryAtSoftware.CleanTests.Core;
+using TryAtSoftware.CleanTests.Core.Interfaces;
+using TryAtSoftware.CleanTests.Core.Utilities;
+using TryAtSoftware.Extensions.Collections;
+
+public class CombinatorialMachineSetup
+{
+    private readonly Dictionary<string, int> _numberOfUtilitiesPerCategory = new ();
+    private readonly Dictionary<string, Dictionary<string, List<string>>> _demandsPerUtility = new ();
+    private readonly Dictionary<string, List<string>> _characteristics = new ();
+
+    public CombinatorialMachineSetup(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+        this.Name = name;
+    }
+
+    public string Name { get; }
+    
+    public CombinatorialMachineSetup WithCategory(string category, int utilitiesCount)
+    {
+        if (string.IsNullOrWhiteSpace(category)) throw new ArgumentNullException(nameof(category));
+        if (utilitiesCount <= 0) throw new ArgumentException("The number of utilities for each category must be at least 1", nameof(utilitiesCount));
+        
+        if (this._numberOfUtilitiesPerCategory.ContainsKey(category)) throw new InvalidOperationException("Category with that name has already been registered.");
+        this._numberOfUtilitiesPerCategory[category] = utilitiesCount;
+        return this;
+    }
+
+    public CombinatorialMachineSetup WithCharacteristics(string category, int utilityId, params string[] characteristics)
+    {
+        this.ValidateUtilityExists(category, utilityId);
+
+        var universalId = ComposeUniversalUtilityId(category, utilityId);
+        if (!this._characteristics.ContainsKey(universalId)) this._characteristics[universalId] = new List<string>();
+        foreach (var characteristic in characteristics.OrEmptyIfNull().IgnoreNullOrWhitespaceValues()) this._characteristics[universalId].Add(characteristic);
+
+        return this;
+    }
+
+    public CombinatorialMachineSetup WithDemands(string utilityCategory, int utilityId, string demandsCategory, params string[] demands)
+    {
+        this.ValidateUtilityExists(utilityCategory, utilityId);
+        this.ValidateCategoryExists(demandsCategory);
+        
+        var universalId = ComposeUniversalUtilityId(utilityCategory, utilityId);
+        if (!this._demandsPerUtility.ContainsKey(universalId)) this._demandsPerUtility[universalId] = new Dictionary<string, List<string>>();
+        if (!this._demandsPerUtility[universalId].ContainsKey(demandsCategory)) this._demandsPerUtility[universalId][demandsCategory] = new List<string>();
+        foreach (var demand in demands.OrEmptyIfNull().IgnoreNullOrWhitespaceValues()) this._demandsPerUtility[universalId][demandsCategory].Add(demand);
+
+        return this;
+    }
+
+    public (CombinatorialMachine CombinatorialMachine, IDictionary<string, ICleanUtilityDescriptor> UtilitiesById) Materialize()
+    {
+        var utilitiesCollection = new CleanTestInitializationCollection<ICleanUtilityDescriptor>();
+        var utilitiesById = new Dictionary<string, ICleanUtilityDescriptor>();
+
+        foreach (var (category, utilitiesCount) in this._numberOfUtilitiesPerCategory)
+        {
+            for (var j = 1; j <= utilitiesCount; j++)
+            {
+                var universalId = ComposeUniversalUtilityId(category, j);
+
+                this._characteristics.TryGetValue(universalId, out var characteristics);
+                ICleanUtilityDescriptor utility = new CleanUtilityDescriptor(category, typeof(int), universalId, isGlobal: false, characteristics, requirements: Enumerable.Empty<string>());
+
+                this._demandsPerUtility.TryGetValue(universalId, out var demandsByCategory);
+                foreach (var (demandCategory, demands) in demandsByCategory.OrEmptyIfNull())
+                {
+                    foreach (var demand in demands) utility.ExternalDemands.Register(demandCategory, demand);
+                }
+
+                utilitiesCollection.Register(category, utility);
+                utilitiesById[utility.Id] = utility;
+            }
+        }
+
+        return (CombinatorialMachine: new CombinatorialMachine(utilitiesCollection), UtilitiesById: utilitiesById);
+    }
+
+    private void ValidateUtilityExists(string category, int utilityId)
+    {
+        this.ValidateCategoryExists(category);
+        
+        var upperBound = this._numberOfUtilitiesPerCategory[category];
+        if (utilityId <= 0 || utilityId > upperBound) throw new ArgumentException($"Invalid utility id. It should be in the range [1, {upperBound}]", nameof(utilityId));
+    }
+
+    private void ValidateCategoryExists(string category)
+    {
+        if (string.IsNullOrWhiteSpace(category)) throw new ArgumentNullException(nameof(category));
+        if (!this._numberOfUtilitiesPerCategory.ContainsKey(category)) throw new InvalidOperationException("Category with that name has not been registered.");
+    }
+
+    private static string ComposeUniversalUtilityId(string category, int utilityId) => $"{category}{utilityId}";
+
+    public override string ToString() => this.Name;
+}
---
+++ Tests/TryAtSoftware.CleanTests.UnitTests/Parametrization/TestParameters.cs
@@ -0,0 +1,45 @@
+﻿namespace TryAtSoftware.CleanTests.UnitTests.Parametrization;
+
+public static class TestParameters
+{
+    public static IEnumerable<CombinatorialMachineSetup> ConstructObservableCombinatorialMachineSetups()
+    {
+        var setup1 = new CombinatorialMachineSetup("Setup #1").WithCategory("A", 3).WithCategory("B", 2).WithCategory("C", 3).WithCategory("D", 3);
+        setup1.WithCharacteristics("C", 1, "exec_A2").WithCharacteristics("C", 3, "exec_A2").WithCharacteristics("D", 1, "exec_C3").WithCharacteristics("D", 2, "exec_B1", "exec_B2", "exec_C3").WithCharacteristics("D", 3, "exec_B1", "exec_B2");
+        setup1.WithDemands("A", 2, "C", "exec_A2").WithDemands("B", 1, "D", "exec_B1").WithDemands("B", 2, "D", "exec_B2").WithDemands("C", 3, "D", "exec_C3");
+        yield return setup1;
+
+        var setup2 = new CombinatorialMachineSetup("Setup #2").WithCategory("A", 2).WithCategory("B", 2).WithCategory("C", 2).WithCategory("D", 2).WithCategory("E", 2);
+        setup2.WithCharacteristics("B", 2, "exec_A1").WithCharacteristics("C", 1, "exec_B2").WithCharacteristics("D", 2, "exec_C1").WithCharacteristics("E", 1, "exec_D2");
+        setup2.WithDemands("A", 1, "B", "exec_A1").WithDemands("B", 2, "C", "exec_B2").WithDemands("C", 1, "D", "exec_C1").WithDemands("D", 2, "E", "exec_D2");
+        yield return setup2;
+
+        var setup3 = new CombinatorialMachineSetup("5 categories; 10 utilities in each; no demands");
+        for (var i = 0; i < 5; i++) setup3.WithCategory(ConstructCategoryName(i), 10);
+        yield return setup3;
+
+        var setup4 = new CombinatorialMachineSetup("10 categories; 3 utilities in each; no demands");
+        for (var i = 0; i < 10; i++) setup4.WithCategory(ConstructCategoryName(i), 3);
+        yield return setup4;
+
+        var setup5 = new CombinatorialMachineSetup("10 categories; 5 utilities in each; demands");
+        for (var i = 0; i < 10; i++) setup5.WithCategory(ConstructCategoryName(i), 5);
+        for (var i = 1; i < 10; i++)
+        {
+            for (var j = 1; j <= 5; j++)
+            {
+                for (var l = 0; l < 3; l++)
+                    setup5.WithCharacteristics(ConstructCategoryName(i), (j + l + 1) % 5 + 1, $"exec_{ConstructCategoryName(i - 1)}{j}");
+            }
+        }
+
+        for (var i = 0; i < 9; i++)
+        {
+            for (var j = 1; j <= 5; j++) setup5.WithDemands(ConstructCategoryName(i), j, ConstructCategoryName(i + 1), $"exec_{ConstructCategoryName(i)}{j}");
+        }
+        
+        yield return setup5;
+    }
+
+    private static string ConstructCategoryName(int letterIndex) => ((char)(letterIndex + 'A')).ToString();
+}
