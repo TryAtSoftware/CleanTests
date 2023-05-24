0d33d0b2c177773ed342b2670e96a73acd200e08
Tony Troeff <tonyslav_troeff@outlook.com>
2023-05-24 16:06:37 +0300

    Added a new attribute that should be used to override execution configurations per test.

---
+++ TryAtSoftware.CleanTests.Core/Attributes/ExecutionConfigurationOverrideAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace TryAtSoftware.CleanTests.Core.Attributes;
+
+using System;
+using TryAtSoftware.CleanTests.Core.Enums;
+using TryAtSoftware.CleanTests.Core.Internal;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class ExecutionConfigurationOverrideAttribute : Attribute
+{
+    private int? _maxDegreeOfParallelism = CleanTestConstants.MaxDegreeOfParallelism;
+
+    public int? MaxDegreeOfParallelism
+    {
+        get => this._maxDegreeOfParallelism;
+        set
+        {
+            if (value.HasValue) Validator.ValidateMaxDegreeOfParallelism(value.Value);
+            this._maxDegreeOfParallelism = value;
+        }
+    }
+}
