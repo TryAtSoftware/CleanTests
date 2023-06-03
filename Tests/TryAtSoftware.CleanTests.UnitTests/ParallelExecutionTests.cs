a83b8985688fb97b7d935af71217f5a16d99fcf2
Tony Troeff <tonyslav_troeff@outlook.com>
2023-06-03 16:18:47 +0300

    Fixed failing tests validating correct max degree of parallelism

--- Tests/TryAtSoftware.CleanTests.UnitTests/ParallelExecutionTests.cs
+++ Tests/TryAtSoftware.CleanTests.UnitTests/ParallelExecutionTests.cs
@@ -1,8 +1,6 @@
 ﻿namespace TryAtSoftware.CleanTests.UnitTests;
 
-using System.Collections.Concurrent;
 using System.Reflection;
-using Moq;
 using TryAtSoftware.CleanTests.Core;
 using TryAtSoftware.CleanTests.Core.Attributes;
 using TryAtSoftware.CleanTests.Core.XUnit;
@@ -10,59 +8,72 @@ using TryAtSoftware.CleanTests.Core.XUnit.Execution;
 using TryAtSoftware.CleanTests.UnitTests.Constants;
 using TryAtSoftware.CleanTests.UnitTests.Extensions;
 using TryAtSoftware.CleanTests.UnitTests.Mocks;
-using TryAtSoftware.Extensions.Reflection.Interfaces;
 using Xunit.Abstractions;
 
 public class ParallelExecutionTests
 {
     private const int TestsCount = 10;
-    
+
     [Theory(Timeout = UnitTestConstants.Timeout)]
-    [InlineData(5)]
-    [InlineData(1)]
-    public async Task Test(int overridesCount)
+    [InlineData(5, 1)]
+    [InlineData(1, 1)]
+    [InlineData(5, 2)]
+    [InlineData(1, 2)]
+    public async Task Test(int overridesCount, int minDegreeOfParallelism)
     {
         var testClass = typeof(ParallelismConfigurationTest);
         var testMethodName = nameof(ParallelismConfigurationTest.Test);
-        
+
         var reflectionMocks = ReflectionMocks.MockReflectionSuite(Assembly.GetExecutingAssembly(), testClass);
         var testComponentMocks = reflectionMocks.MockTestComponentsSuite();
 
         var overrides = new ExecutionConfigurationOverrideAttribute[overridesCount];
-        for (var i = 0; i < overrides.Length; i++) overrides[i] = new ExecutionConfigurationOverrideAttribute { MaxDegreeOfParallelism = overridesCount - i };
+        for (var i = 0; i < overrides.Length; i++) overrides[i] = new ExecutionConfigurationOverrideAttribute { MaxDegreeOfParallelism = minDegreeOfParallelism + overridesCount - (i + 1) };
 
-        var mockedHierarchyScanner = new Mock<IHierarchyScanner>();
-        mockedHierarchyScanner.Setup(x => x.ScanForAttribute<ExecutionConfigurationOverrideAttribute>(It.Is<MethodInfo>(mi => mi.Name == nameof(ParallelismConfigurationTest.Test)))).Returns<MemberInfo>(_ => new[] { new ExecutionConfigurationOverrideAttribute { MaxDegreeOfParallelism = 1 } });
         var assemblyData = new CleanTestAssemblyData { HierarchyScanner = ReflectionMocks.MockHierarchyScanner(testClass, testMethodName, overrides) };
 
         var testCases = await reflectionMocks.AssemblyInfo.DiscoverTestCasesAsync(assemblyData, testComponentMocks);
         var cleanTestAssemblyRunner = new CleanTestAssemblyRunner(testComponentMocks.TestAssembly, testCases, testComponentMocks.DiagnosticMessageSink, testComponentMocks.ExecutionMessageSink, testComponentMocks.TestFrameworkExecutionOptions, assemblyData);
 
+        ParallelismConfigurationTest.ResetState();
         var executionResult = await cleanTestAssemblyRunner.RunAsync();
         Assert.NotNull(executionResult);
+
         Assert.Equal(0, executionResult.Failed);
         Assert.Equal(0, executionResult.Skipped);
         Assert.Equal(TestsCount, executionResult.Total);
 
-        var threadInfo = Assert.Single(ParallelismConfigurationTest.UsedThreads);
-        Assert.Equal((1 << TestsCount) - 1, threadInfo.Value);
+        Assert.Equal(minDegreeOfParallelism, ParallelismConfigurationTest.MaxExecutedTestsInParallelCount);
     }
-    
+
     private class ParallelismConfigurationTest : CleanTest
     {
-        public static ConcurrentDictionary<int, int> UsedThreads { get; } = new ();
+        private static readonly object _lock = new ();
+        private static int _currentExecutedTestsInParallelCount;
+
+        public static int MaxExecutedTestsInParallelCount { get; private set; }
 
         public ParallelismConfigurationTest(ITestOutputHelper testOutputHelper)
             : base(testOutputHelper)
         {
         }
 
+        public static void ResetState() => MaxExecutedTestsInParallelCount = 0;
+
         [CleanTheory]
         [MemberData(nameof(GetMemberData))]
-        public void Test(int id)
+        public async Task Test(int id)
         {
-            var val = UsedThreads.AddOrUpdate(Environment.CurrentManagedThreadId, _ => id, (_, v) => v | id);
-            Assert.True(val > 0);
+            lock (_lock)
+            {
+                _currentExecutedTestsInParallelCount++;
+                MaxExecutedTestsInParallelCount = Math.Max(MaxExecutedTestsInParallelCount, _currentExecutedTestsInParallelCount);
+            }
+
+            await Task.Delay(20);
+            Assert.True(id > 0);
+
+            lock (_lock) _currentExecutedTestsInParallelCount--;
         }
 
         public static IEnumerable<object[]> GetMemberData()
