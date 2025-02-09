c3438f12575fc778e36e2b53e7c84de222ca0a9c
Tony Troeff <tonyslav_troeff@outlook.com>
2025-02-10 00:30:46 +0200

    Added tests ensuring that outer demands are targetting only the immediate higher hierarchical level.

--- Tests/TryAtSoftware.CleanTests.UnitTests/ConstructionManagerTests.cs
+++ Tests/TryAtSoftware.CleanTests.UnitTests/ConstructionManagerTests.cs
@@ -11,8 +11,7 @@ using TryAtSoftware.Randomizer.Core.Helpers;
 
 public class ConstructionManagerTests
 {
-    [Theory(Timeout = UnitTestConstants.Timeout)]
-    [MemberData(nameof(GetDependenciesManagerSetups))]
+    [Theory(Timeout = UnitTestConstants.Timeout), MemberData(nameof(GetDependenciesManagerSetups))]
     public Task DependencyGraphsShouldBeConstructedSuccessfully(EnvironmentSetup setup, string pathToExpectedResult)
     {
         var assemblyTestData = setup.MaterializeAsAssemblyData();
@@ -41,32 +40,32 @@ public class ConstructionManagerTests
         
         for (var i = 1; i <= count; i++)
         {
-            setup.WithCharacteristics("Q", i, $"C{i}");
-            setup.WithOuterDemands("N1", i, "Q", $"C{i}");
+            setup.WithCharacteristics("Q", i, $"O{i}");
+            setup.WithOuterDemands("N1", i, "Q", $"O{i}");
         }
 
         var assemblyTestData = setup.MaterializeAsAssemblyData();
         var manager = new ConstructionManager(assemblyTestData);
 
-        var utilitiesMatrix = ExtractUtilitiesForOuterDemandsTesting(assemblyTestData, depth: 1, count);
+        var matrix = ExtractMatrixForOuterDemandsTesting(assemblyTestData, depth: 1, count);
 
         for (var i = 0; i < count; i++)
         {
-            var utilityIds = new[] { utilitiesMatrix[0][0].Id, utilitiesMatrix[^1][i].Id };
+            var utilityIds = new[] { matrix[0][0].Id, matrix[^1][i].Id };
             var constructionGraphs = manager.BuildIndividualConstructionGraphs(utilityIds);
             Assert.Single(constructionGraphs);
             Assert.Equal(2, constructionGraphs[0].Length);
             
             var penultConstructionGraph = constructionGraphs[0][0];
-            Assert.Equal(utilitiesMatrix[0][0].Id, penultConstructionGraph.Id);
+            Assert.Equal(matrix[0][0].Id, penultConstructionGraph.Id);
             Assert.Single(penultConstructionGraph.Dependencies);
         
             var qConstructionGraph = constructionGraphs[0][1];
-            Assert.Equal(utilitiesMatrix[^1][i].Id, qConstructionGraph.Id);
+            Assert.Equal(matrix[^1][i].Id, qConstructionGraph.Id);
             Assert.Empty(qConstructionGraph.Dependencies);
         
             var lastConstructionGraph = penultConstructionGraph.Dependencies[0];
-            Assert.Equal(utilitiesMatrix[1][i].Id, lastConstructionGraph.Id);
+            Assert.Equal(matrix[1][i].Id, lastConstructionGraph.Id);
             Assert.Empty(lastConstructionGraph.Dependencies);
         }
     }
@@ -80,18 +79,18 @@ public class ConstructionManagerTests
 
         for (var i = 1; i <= count; i++)
         {
-            setup.WithCharacteristics("Q", i, $"C{i}");
-            setup.WithOuterDemands("N1", i, "Q", $"D{i}");
+            setup.WithCharacteristics("Q", i, $"O{i}");
+            setup.WithOuterDemands("N1", i, "Q", $"X{i}");
         }
 
         var assemblyTestData = setup.MaterializeAsAssemblyData();
         var manager = new ConstructionManager(assemblyTestData);
 
-        var utilitiesMatrix = ExtractUtilitiesForOuterDemandsTesting(assemblyTestData, depth: 1, count);
+        var matrix = ExtractMatrixForOuterDemandsTesting(assemblyTestData, depth: 1, count);
 
         for (var i = 0; i < count; i++)
         {
-            var utilityIds = new[] { utilitiesMatrix[0][0].Id, utilitiesMatrix[^1][i].Id };
+            var utilityIds = new[] { matrix[0][0].Id, matrix[^1][i].Id };
             var constructionGraphs = manager.BuildIndividualConstructionGraphs(utilityIds);
             Assert.Empty(constructionGraphs);
         }
@@ -106,46 +105,16 @@ public class ConstructionManagerTests
 
         for (var i = 1; i <= count; i++)
         {
-            setup.WithCharacteristics("Q", i, $"C{i}");
-            setup.WithOuterDemands($"N{depth}", i, "Q", $"C{i}");
+            setup.WithCharacteristics("Q", i, $"O{i}");
+            setup.WithOuterDemands($"N{depth}", i, "Q", $"O{i}");
         }
 
         var assemblyTestData = setup.MaterializeAsAssemblyData();
         var manager = new ConstructionManager(assemblyTestData);
 
-        var utilitiesMatrix = ExtractUtilitiesForOuterDemandsTesting(assemblyTestData, depth, count);
-
-        var utilityIds = utilitiesMatrix[0].Select(x => x.Id);
-        var constructionGraphs = manager.BuildIndividualConstructionGraphs(utilityIds);
-        
-        Assert.Equal(count, constructionGraphs.Length);
-        for (var i = 0; i < count; i++)
-        {
-            var analyzedGraph = Assert.Single(constructionGraphs[i]);
-
-            for (var j = 0; j < depth - 2; j++)
-            {
-                Assert.Equal(utilitiesMatrix[j][0].Id, analyzedGraph.Id);
-                Assert.Single(analyzedGraph.Dependencies);
-
-                analyzedGraph = analyzedGraph.Dependencies[0];
-            }
-            
-            Assert.Equal(utilitiesMatrix[depth - 2][0].Id, analyzedGraph.Id);
-            Assert.Equal(2, analyzedGraph.Dependencies.Count);
-        
-            var penultConstructionGraph = analyzedGraph.Dependencies[0];
-            Assert.Equal(utilitiesMatrix[depth - 1][0].Id, penultConstructionGraph.Id);
-            Assert.Single(penultConstructionGraph.Dependencies);
-        
-            var qConstructionGraph = analyzedGraph.Dependencies[1];
-            Assert.Equal(utilitiesMatrix[^1][i].Id, qConstructionGraph.Id);
-            Assert.Empty(qConstructionGraph.Dependencies);
-        
-            var lastConstructionGraph = penultConstructionGraph.Dependencies[0];
-            Assert.Equal(utilitiesMatrix[depth][i].Id, lastConstructionGraph.Id);
-            Assert.Empty(lastConstructionGraph.Dependencies);
-        }
+        var matrix = ExtractMatrixForOuterDemandsTesting(assemblyTestData, depth, count);
+        var constructionGraphs = manager.BuildIndividualConstructionGraphs(matrix[0].Select(x => x.Id));
+        AssertCorrectConstructionWithOuterDemandsAtDeeperLevels(constructionGraphs, matrix, depth, count);
     }
 
     [Theory, MemberData(nameof(GetDepthMembers))]
@@ -157,22 +126,52 @@ public class ConstructionManagerTests
 
         for (var i = 1; i <= count; i++)
         {
-            setup.WithCharacteristics("Q", i, $"C{i}");
-            setup.WithOuterDemands($"N{depth}", i, "Q", $"D{i}");
+            setup.WithCharacteristics("Q", i, $"O{i}");
+            setup.WithOuterDemands($"N{depth}", i, "Q", $"X{i}");
         }
 
         var assemblyTestData = setup.MaterializeAsAssemblyData();
         var manager = new ConstructionManager(assemblyTestData);
 
-        var utilitiesMatrix = ExtractUtilitiesForOuterDemandsTesting(assemblyTestData, depth, count);
-
-        var utilityIds = utilitiesMatrix[0].Select(x => x.Id);
-        var constructionGraphs = manager.BuildIndividualConstructionGraphs(utilityIds);
+        var matrix = ExtractMatrixForOuterDemandsTesting(assemblyTestData, depth, count);
+        var constructionGraphs = manager.BuildIndividualConstructionGraphs(matrix[0].Select(x => x.Id));
         Assert.Empty(constructionGraphs);
     }
 
     /// <summary>
-    /// |N0| -> |N1| -> ... -> |N{depth - 2}| -> |N{depth - 1}| -> |N{depth}|<br/>
+    /// N{depth} defines unsatisfiable outer demands towards N0, N1, ..., N{depth - 2}.
+    /// These unsatisfiable demands do not impact the construction of individual graphs,
+    /// as the referenced utilities are not located within the immediate higher hierarchical level.
+    /// </summary>
+    [Theory, MemberData(nameof(GetDepthMembers))]
+    public void OuterDemandsShouldOnlyTargetTheImmediateHigherLevel(int depth)
+    {
+        var setup = new EnvironmentSetup("outer_demands_env");
+        var count = RandomizationHelper.RandomInteger(3, 10);
+        PrepareForOuterDemandsTesting(setup, depth, count);
+
+        for (var i = 1; i <= count; i++)
+        {
+            setup.WithCharacteristics("Q", i, $"O{i}");
+            setup.WithOuterDemands($"N{depth}", i, "Q", $"O{i}");
+
+            for (var j = 0; j < depth - 1; j++)
+            {
+                setup.WithCharacteristics($"N{j}", 1, $"O{i}");
+                setup.WithOuterDemands($"N{depth}", i, $"N{j}", $"X{i}");
+            }
+        }
+
+        var assemblyTestData = setup.MaterializeAsAssemblyData();
+        var manager = new ConstructionManager(assemblyTestData);
+
+        var matrix = ExtractMatrixForOuterDemandsTesting(assemblyTestData, depth, count);
+        var constructionGraphs = manager.BuildIndividualConstructionGraphs(matrix[0].Select(x => x.Id));
+        AssertCorrectConstructionWithOuterDemandsAtDeeperLevels(constructionGraphs, matrix, depth, count);
+    }
+
+    /// <summary>
+    /// |N0| -> |N1| -> ... -> |N{depth - 2}| -> |N{depth - 1}| -> |N{depth}|
     ///                                       -> |Q| -----------------^
     ///
     /// N{depth} defines outer demands towards Q
@@ -194,7 +193,7 @@ public class ConstructionManagerTests
     /// - The first `depth + 1` entries are reserved for the `N#` categories.
     /// - The last entry is reserved for the `Q` category.
     /// </summary>
-    private static ICleanUtilityDescriptor[][] ExtractUtilitiesForOuterDemandsTesting(CleanTestAssemblyData assemblyTestData, int depth, int count)
+    private static ICleanUtilityDescriptor[][] ExtractMatrixForOuterDemandsTesting(CleanTestAssemblyData assemblyTestData, int depth, int count)
     {
         // NOTE: There are depth + 1 `N#` categories and `Q`.
         var matrix = new ICleanUtilityDescriptor[depth + 2][];
@@ -205,6 +204,38 @@ public class ConstructionManagerTests
         for (var i = depth; i < matrix.Length; i++) Assert.Equal(count, matrix[i].Length);
 
         return matrix;
+    }
+
+    private static void AssertCorrectConstructionWithOuterDemandsAtDeeperLevels(IndividualCleanUtilityConstructionGraph[][] constructionGraphs, ICleanUtilityDescriptor[][] matrix, int depth, int count)
+    {
+        Assert.Equal(count, constructionGraphs.Length);
+        for (var i = 0; i < count; i++)
+        {
+            var analyzedGraph = Assert.Single(constructionGraphs[i]);
+
+            for (var j = 0; j < depth - 2; j++)
+            {
+                Assert.Equal(matrix[j][0].Id, analyzedGraph.Id);
+                Assert.Single(analyzedGraph.Dependencies);
+
+                analyzedGraph = analyzedGraph.Dependencies[0];
+            }
+            
+            Assert.Equal(matrix[depth - 2][0].Id, analyzedGraph.Id);
+            Assert.Equal(2, analyzedGraph.Dependencies.Count);
+        
+            var penultConstructionGraph = analyzedGraph.Dependencies[0];
+            Assert.Equal(matrix[depth - 1][0].Id, penultConstructionGraph.Id);
+            Assert.Single(penultConstructionGraph.Dependencies);
+        
+            var qConstructionGraph = analyzedGraph.Dependencies[1];
+            Assert.Equal(matrix[^1][i].Id, qConstructionGraph.Id);
+            Assert.Empty(qConstructionGraph.Dependencies);
+        
+            var lastConstructionGraph = penultConstructionGraph.Dependencies[0];
+            Assert.Equal(matrix[depth][i].Id, lastConstructionGraph.Id);
+            Assert.Empty(lastConstructionGraph.Dependencies);
+        }
     }
 
     public static TheoryData<EnvironmentSetup, string> GetDependenciesManagerSetups()
