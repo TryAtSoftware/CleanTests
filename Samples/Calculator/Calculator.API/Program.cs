465c5a39cba846fd54311cd4281be05f532d2ab6
Tony Troeff <tonyslav_troeff@outlook.com>
2026-06-05 23:12:07 +0300

    Respondid to automated PR comments.

--- Samples/Calculator/Calculator.API/Program.cs
+++ Samples/Calculator/Calculator.API/Program.cs
@@ -13,7 +13,3 @@ var app = builder.Build();
 
 app.MapControllers();
 app.Run();
-
-#pragma warning disable S1118 // This class is needed for API tests
-public partial class Program {}
-#pragma warning restore S1118
